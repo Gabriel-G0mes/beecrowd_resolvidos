32855f51eaaafa23e6020967fd7e77c0632cf49b
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-02-07 08:58:02 -0300

    resolvidos 07/02

---
+++ src/br/com/gabriel/beecrowd/bee1114.java
@@ -0,0 +1,20 @@
+package br.com.gabriel.beecrowd;
+import java.util.Scanner;
+
+public class bee1114 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int x = input.nextInt();
+
+        while (x != 2002) {
+            System.out.println("Senha Invalida");
+            x = input.nextInt();
+        }
+
+        System.out.println("Acesso Permitido");
+
+        input.close();
+    }
+}
+
