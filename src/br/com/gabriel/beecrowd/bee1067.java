d19abef68f0987f8338c5a39bd47727f6568cda1
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-02-09 19:46:38 -0300

    resolvido 1067

--- src/br/com/gabriel/beecrowd/bee1067.java
+++ src/br/com/gabriel/beecrowd/bee1067.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 
 public class bee1067 {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
 
         int x = input.nextInt();
