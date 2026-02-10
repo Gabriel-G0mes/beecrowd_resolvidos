4ba1fee477bc02591ad9385887a17785c854620e
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-02-10 05:31:26 -0300

    resolvido 1072

---
+++ src/br/com/gabriel/beecrowd/bee1072.java
@@ -0,0 +1,27 @@
+package br.com.gabriel.beecrowd;
+import java.util.Scanner;
+
+public class bee1072 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+        int out = 0;
+        int in = 0;
+        int X = 0;
+
+        for(int i=0; i<N; i++) {
+            X = input.nextInt();
+            if (X >= 10 && X <= 20) {
+                in += 1;
+            } else {
+                out += 1;
+            }
+        }
+
+            System.out.println(in + " in");
+            System.out.println(out + " out");
+
+            input.close();
+        }
+    }
