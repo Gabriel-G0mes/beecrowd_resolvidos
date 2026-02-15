faf467d378b6038ba45116240d4953cfff96e6c9
Gabriel-G0mes <gabrieelgomes73@gmail.com>
2026-02-15 10:19:02 -0300

    resolvido 1157

---
+++ src/br/com/gabriel/beecrowd/bee1157.java
@@ -0,0 +1,18 @@
+package br.com.gabriel.beecrowd;
+import java.util.Scanner;
+
+public class bee1157 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int N = input.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            if(N % i == 0) {
+                System.out.println(i);
+            }
+        }
+
+        input.close();
+    }
+}
